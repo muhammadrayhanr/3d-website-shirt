cd63e079fe6f2039da3af457aee63cc5b8b7ccc4
muhammadrayhanr <mrayhanr4869@gmail.com>
2023-06-18 18:49:58 +0700

    setup awal dan menyiapkan home page

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import state from '../store';
+import { useSnapshot } from 'valtio';
+
+const CustomButton = ({ type, title, customStyles, handleClick }) => {
+  const snap = useSnapshot(state);
+  const generateStyles = (type) => {
+    if (type === 'filled') {
+      return {
+        backgroundColor: snap.color,
+        color: '#fff',
+      };
+    }
+  };
+  return (
+    <button className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`} style={generateStyles(type)} onClick={handleClick}>
+      {title}
+    </button>
+  );
+};
+
+export default CustomButton;
